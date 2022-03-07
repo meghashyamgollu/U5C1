a25d70672ebf74e98b75de98684c7acc7887edb8
shyam g <meghashyam.g@gmail.com>
2022-03-07 10:30:22 +0530

    items done

--- inventory/src/App.jsx
+++ inventory/src/App.jsx
@@ -1,12 +1,28 @@
-import Books from './components/Counter';
+import { Books } from './components/Counter';
+import { Pens } from './components/Counter';
+import { Notebooks } from './components/Counter';
+import { Inkpens } from './components/Counter';
 import './App.css';
 
 function App() {
-  let books = 13
+  let books = 13;
+  let pens = 10;
+  let notebooks = 44;
+  let inkpens = 78;
   return (
     <div className="App">
+      Inventory
       <div className="items">
         <Books value={books}/>
+      </div>
+      <div className='items'>
+        <Pens value= {pens}/>
+      </div>
+      <div className='items'>
+        <Notebooks value = {notebooks}/>
+      </div>
+      <div className='items'>
+        <Inkpens value= {inkpens}/>
       </div>
     </div>
   );
--- inventory/src/components/Counter.jsx
+++ inventory/src/components/Counter.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
 function Books(props){
-    const [books, updateCount] = useState(13);
+    const [books, updateCount] = useState(props.value);
 
     const updateBooks = (value) => {
+        if(books===0){
+            return;
+        }
         updateCount(books + value);
     }
 
@@ -21,4 +24,76 @@ function Books(props){
     )
 }
 
-export default Books
+function Pens(props){
+    const [pens, updateCount] = useState(props.value);
+
+    const updatePens = (value) => {
+        if(pens===0){
+            return;
+        }
+        updateCount(pens + value);
+    }
+
+    return(
+        <div>
+            <span>Pens:</span>
+            <button className="addPen" onClick = {
+                () => updatePens(1)
+            } >+</button>
+            <button className="remPen" onClick = {
+                () => updatePens(-1)
+            } >-</button>
+            <span className="totalPens">{pens}</span>
+        </div>
+    )
+}
+
+function Notebooks(props){
+    const [notebooks, updateCount] = useState(props.value);
+
+    const updateNotebooks = (value) => {
+        if(notebooks===0){
+            return;
+        }
+        updateCount(notebooks + value);
+    }
+
+    return(
+        <div>
+            <span>Notebooks:</span>
+            <button className="addNotebook" onClick = {
+                () => updateNotebooks(1)
+            } >+</button>
+            <button className="remNotebook" onClick = {
+                () => updateNotebooks(-1)
+            } >-</button>
+            <span className="totalNotebooks">{notebooks}</span>
+        </div>
+    )
+}
+
+function Inkpens(props){
+    const [inkpens, updateCount] = useState(props.value);
+
+    const updateInkpens = (value) => {
+        if(inkpens===0){
+            return;
+        }
+        updateCount(inkpens + value);
+    }
+
+    return(
+        <div>
+            <span>Inkpens:</span>
+            <button className="addInkpen" onClick = {
+                () => updateInkpens(1)
+            } >+</button>
+            <button className="remInkpen" onClick = {
+                () => updateInkpens(-1)
+            } >-</button>
+            <span className="totalInkpens">{inkpens}</span>
+        </div>
+    )
+}
+
+export  {Books, Pens, Notebooks, Inkpens};
